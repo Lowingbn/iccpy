054a0a44d2f5f2c2bf53acfef65ef30ad5c0e6b2
Peter Creasey <p.e.creasey.00@gmail.com>
2010-06-11 16:13:15 +0000

    ignore pixels outside the box, allow a weighted value (like histogram)

--- graphs/cloud_in_cell.py
+++ graphs/cloud_in_cell.py
@@ -1,18 +1,22 @@
-from numpy import uint64, clip, searchsorted, bincount, maximum, minimum, diff, float64, zeros
+from numpy import uint64, clip, searchsorted, bincount, maximum, minimum, diff, float64, zeros, equal, where, zeros_like
 
-def cloud_in_cell(x,y, pts_x, pts_y, min_val=False):
+def cloud_in_cell(x,y, pts_x, pts_y, pts_vals=None, min_val=False):
     """
     creates a grid of cells with borders x[:] and y[:]
-    counts the fraction of particles in each cell, using nearest cell approximation
+    counts the fraction of particles in each cell, using cloud-in-cell approximation
 
     x - x borders
     y - y borders
     pts_x - x values of the points
     pts_y - y values of the points
+    pts_vals - values for each point (default=None). If set then the grid contains the average value for the cells
+
     min_val=False - if True then set the minimum number in each cell as 1, (good for log plots)
 
     """
-    res = zeros((x.size* y.size), dtype=float64)
+    density = zeros((x.size*y.size), dtype=float64)
+    if pts_vals is not None:
+        values = zeros_like(density)
 
     # find the surrounding cell borders
     idx_x = searchsorted(x, pts_x)
@@ -29,24 +33,43 @@ def cloud_in_cell(x,y, pts_x, pts_y, min_val=False):
     dy = diff(y)[minimum(idx_y0, y.size-2)]
     frac_y = clip((pts_y - y[idx_y0]) / dy, 0, 1)
 
+    outside_box = equal(idx_x,0) | equal(idx_x,x.size) | equal(idx_y, 0) | equal(idx_y, y.size)
+ 
 
     idx00 = idx_y0 * x.size + idx_x0
     idx01 = idx_y0 * x.size + idx_x1
     idx10 = idx_y1 * x.size + idx_x0
     idx11 = idx_y1 * x.size + idx_x1
 
-    w00 = (1-frac_y) * (1-frac_x)
-    w01 = (1-frac_y) * frac_x
-    w10 = frac_y * (1-frac_x)
-    w11 = frac_y * frac_x
+    w00 = where(outside_box, 0, (1-frac_y) * (1-frac_x))
+    w01 = where(outside_box, 0, (1-frac_y) * frac_x)
+    w10 = where(outside_box, 0, frac_y * (1-frac_x))
+    w11 = where(outside_box, 0, frac_y * frac_x)
 
     for idx, weights in zip((idx00,idx01,idx10,idx11), (w00,w01,w10,w11)):
         b = bincount(idx, weights)
-        res[:b.size] += b
+
+        density[:b.size] += b
+
+        if pts_vals is not None:
+            b = bincount(idx, weights * pts_vals)
+            values[:b.size] += b
+
+
+    res = density
 
     if min_val:
         res = maximum(res, 1.0)
 
-    res = res.reshape((y.size, x.size)) / res.sum()
+    if pts_vals is not None:
+        res = values / res
+        if min_val:
+            res = maximum(res, pts_vals.min())
+    else:
+        # normalise
+        res = res / res.sum()
+
+
+    res = res.reshape((y.size, x.size))
 
     return res
