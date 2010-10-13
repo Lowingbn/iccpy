50f5659f344d3f504a46768b7126b2960ef2f724
Ben Lowing <Lowingbn@gmail.com>
2010-10-13 16:55:21 +0000

    Improved snapshot reading

--- gadget/binary_snapshot_io.py
+++ gadget/binary_snapshot_io.py
@@ -1,10 +1,6 @@
 import numpy as np
 from numpy import uint32, uint64, float64, float32
 
-mpc = 3.0857e24 # cm 
-km = 1.0e5 # cm
-msun = 2.0e33 # solar mass in g
-
 header_names = ('npart', 'mass', 'time', 'redshift', 'flag_sfr', 'flag_feedback', 'npartTotal', 'flag_cooling', \
                 'num_files', 'boxsize', 'omega0', 'omegaLambda', 'hubble0', 'flag_stellarage', 'flag_metals', \
                 'npartTotalHighWord', 'flag_entropy_instead_u', 'flag_doubleprecision', 'buffer')
@@ -13,8 +9,7 @@ header_sizes = ((uint32, 6), (float64, 6), (float64, 1), (float64, 1), (uint32, 
              (uint32, 6), (uint32, 1), (uint32, 1), (float64, 1), (float64, 1), (float64, 1), \
              (float64, 1), (uint32, 1), (uint32, 1), (uint32, 6), (uint32, 1), (uint32, 1), (np.uint8, 56))
 
-
-def read_snapshot_file(filename, physical=False, ics=False, cooling=False):
+def read_snapshot_file(filename, gas=False, ics=False, cooling=False):
     """ Reads a binary gadget file """
     f = open(filename, mode='rb')
 
@@ -60,10 +55,9 @@ def read_snapshot_file(filename, physical=False, ics=False, cooling=False):
                 pmass.append(mass)
         else:
             pmass.append(0)
-
-    ngas = nparts[0]
     
-    if ngas > 0:
+    if gas:
+        ngas = nparts[0]
         res['therm'] = readu(f, float32, ngas)
         if not ics:
             res['rho'] = readu(f, float32, ngas)
@@ -80,34 +74,7 @@ def read_snapshot_file(filename, physical=False, ics=False, cooling=False):
         else:
             res['sml'] = 0.0
 
-
-    h = header['hubble0']
-    z = header['redshift']
-
-    lunit = 1.0 / h # Mpc
-    munit = 1.e10 / h
-    vunit = np.sqrt(1.0 / (1 + z)) * 1.e5 / km                    # km/s
-
-    print ' units: I assume that lunit=Mpc/h, munit=10^10 modot0/h, v_unit=km/s '
-
-    if physical:
-        print ' converting to physical Mpc, km/s, M_odot, g/cm^3'
-        pos = pos * lunit / (1 + z)
-        if ngas > 0:
-                res['sml'] = res['sml'] * lunit / (1. + z)
-                res['rho'] = res['rho'] * munit / (lunit * mpc / (1 + z)) ** 3.0 * msun
-                res['therm'] = res['therm'] * vunit ** 2 * (1 + z)
-
-        vel = vel * vunit / np.sqrt(1 + z)
-        mass = mass * munit
-        pmass = [p * munit for p in pmass]
-
-        munit = msun
-        lunit = mpc
-        vunit = km
-
     f.close()
-
 
     res['pos'] = pos
     res['vel'] = vel
@@ -116,7 +83,7 @@ def read_snapshot_file(filename, physical=False, ics=False, cooling=False):
     
     return header, res
 
-def write_snapshot_file(filename, header, data, physical=True, cooling=False, ics=False, gamma=1.66667):
+def write_snapshot_file(filename, header, pos, vel, ids, masses=None, extra_data=None):
     """ Write a binary gadget file (unformatted fortran) """
 
     # do some checks on the header
@@ -131,71 +98,22 @@ def write_snapshot_file(filename, header, data, physical=True, cooling=False, ic
             print msg
             raise Exception(msg)
 
-
     # do some checks on the data
     nparts = header['npart']
     total = np.array(nparts).sum()
-    ngas = nparts[0]
+    
+    mass_len = sum([ num for num, mass in zip(nparts, header['mass']) if mass==0 ])
+    if mass_len!=len(masses):
+        raise Exception('bad mass values')
 
-    for num, mass, pmass in zip(nparts, header['mass'], data['mass']):
-        if num > 0 and mass == 0:
-            if len(pmass) != num:
-                raise Exception('bad mass values')
-
+    #should we write in single or double precision
     if header['flag_doubleprecision']==0:
-        init = [('pos', float32, total * 3), ('vel', float32, total * 3), ('id', uint32, total)]
+        precision = float64
     else:
-        init = [('pos', float64, total * 3), ('vel', float64, total * 3), ('id', uint64, total)]
-        
-    a = []
-    if ngas > 0:
-        a.append(('therm', float32, ngas))
-        
-        if not ics:
-            a.append(('rho', float32, ngas))
-            
-            if cooling:
-                a.append(('Ne', float32, ngas))
-
-        if cooling:
-            a.append(('NHI', float32, ngas))
-            a.append(('NHeI', float32, ngas))
-            a.append(('NHeIII', float32, ngas))
-
-    if not ics:
-        a.append(('sml', float32, ngas))
-
-
-    for name, dtype, size in a:
-        if data[name].size != size:
-            raise Exception('%s has wrong size' % name)
+        precision = float64
 
     # ok so far, lets write the file
     f = open(filename, 'wb')
-
-
-    if physical:
-        # convert to co-moving
-        print ' converting to physical Mpc, km/s, M_odot, g/cm^3'
-        h = header['hubble0']
-        z = header['redshift']
-
-        lunit = 1.0 / h # Mpc
-        munit = 1.e10 / h
-        vunit = np.sqrt(1.0 / (1 + z)) * 1.e5 / km                    # km/s
-
-        data['pos'] = data['pos'] * (1 + z) / lunit
-        if ngas > 0:
-                data['sml'] = data['sml'] / (lunit / (1. + z))
-                data['rho'] = data['rho'] / (munit / (lunit * mpc / (1 + z)) ** 3.0 * msun)
-                if header['flag_entropy_instead_u'] == 0:
-                    data['therm'] = data['therm'] / (vunit ** 2 * (1 + z))
-                else:
-                    data['therm'] = data['therm'] / (vunit ** 2 * (1 + z)) * (munit / (lunit * mpc / (1 + z)) ** 3.0 * msun) ** (gamma - 1)
-
-        data['vel'] = data['vel'] / (vunit / np.sqrt(1 + z))
-        header['mass'] = [m / munit for m in header['mass']]
-        data['mass'] = [p / munit for p in data['mass']]
 
     # write the header
     np.array(256, uint32).tofile(f)
@@ -206,16 +124,15 @@ def write_snapshot_file(filename, header, data, physical=True, cooling=False, ic
     np.array(256, uint32).tofile(f)
     
     # write the data
-    for name, dtype, size in init:
-        writeu(f, data[name].astype(dtype))
-
-    # the masses
-    for mass, pmass in zip(header['mass'], data['mass']):
-        if mass == 0:
-            writeu(f, pmass.astype(float32))
-
-    for name, dtype, size in a:
-        writeu(f, data[name].astype(dtype))
+    writeu(f, pos.astype(precision))
+    writeu(f, vel.astype(precision))
+    writeu(f, ids)
+    
+    if mass_len>0:
+        writeu(f, masses.astype(precision))
+        
+    for data in extra_data:
+        writeu(f, data.astype(precision))
  
     # all done!
     f.close()
@@ -223,9 +140,7 @@ def write_snapshot_file(filename, header, data, physical=True, cooling=False, ic
 def read_snapshot_header(filename):
     """ Reads just the header from a binary GADGET file """
     f = open(filename, mode='rb')
-    
     header = read_header(f)
-    
     f.close()
     
     return header
@@ -234,10 +149,8 @@ def read_header(f):
     """ Read the binary GADGET header file into a dictionary """
     assert(np.fromfile(f, uint32, 1)[0] == 256) # 256 byte header
 
-
     header = dict(((name, np.fromfile(f, dtype=size[0], count=size[1])) \
                                          for name, size in zip(header_names, header_sizes)))
-
 
     assert(np.fromfile(f, uint32, 1)[0] == 256)
     return header
@@ -266,7 +179,7 @@ def readIDs(f, count=None):
     return arr   
 
 def readu(f, dtype=None, count=None):
-    """ Read a numpy array from the unformatted fortran file f """
+    """ Read a numpy array from the unformatted fortran file f """  
     data_size = np.fromfile(f, uint32, 1)[0]
     read_size = data_size
 
@@ -291,7 +204,6 @@ def readu(f, dtype=None, count=None):
 
 def writeu(f, arr=None):
     """ Write a numpy array to the unformatted fortran file f """
-
     if arr is None:
         np.array(0, dtype=np.uint64).tofile(f)
         return
--- gadget/snapshot.py
+++ gadget/snapshot.py
@@ -1,5 +1,6 @@
 import binary_snapshot_io
 import os.path
+import numpy as np
 
 def _get_filename(directory, file, snapnum=0, filenum=0):
     if filenum==0 and snapnum==0:
@@ -37,3 +38,19 @@ def load_snapshot_files(directory, file, snapshot_num):
         filename = _get_filename(directory, file, snapshot_num, i)
         header, res = binary_snapshot_io.read_snapshot_file(filename, False)
         yield header, res
+
+def convert_to_physical(header, res):
+    """ convert data from a Gadget snapshot into physical coordinates """
+    h = header['hubble0']
+    z = header['redshift']
+    
+    res['pos'] *= (1+z) / h
+    res['vel'] = res['vel'] * np.sqrt(1+z) #+ H_a * res['pos']
+    res['mass'] /= h
+    
+    if 'sml' in res: res['sml'] *= (1+z) / h
+
+def convert_to_comoving(header, res):
+    """ convert data from a Gadget snapshot into co-moving coordinates """
+    h = header['hubble0']
+    z = header['redshift']
