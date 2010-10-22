59819dcd6f84877d2abdbb5b4ce2b64a43be96e7
Ben Lowing <Lowingbn@gmail.com>
2010-10-22 09:53:49 +0000

    Bug fix

--- iccpy/cosmology/constants.py
+++ iccpy/cosmology/constants.py
@@ -12,10 +12,10 @@ def _hubble_func(a):
     return np.sqrt(OMEGA_K0/(a*a) + OMEGA_M0/(a**3) + OMEGA_R0/(a**4) + OMEGA_L0);
 
 def hubble_0(h=h):
-    return h * 3.2407764868e-18
+    return h * 3.2407764868e-18 * iccpy.constants.UNIT_TIME
     
 def hubble_param(a, h=h):
-    return hubble_0(h) * iccpy.constants.UNIT_TIME * _hubble_func(a)
+    return hubble_0(h) * _hubble_func(a)
     
 def omega_m(a):
     h = _hubble_func(a)
