65a08ed6676c9934415016c29746f11957a2c8ae
Peter Creasey <p.e.creasey.00@gmail.com>
2011-07-04 17:24:42 +0000

    uniform_grid function to make a (lazy) uniform grid for given variables

--- iccpy/flash/read_flash.py
+++ iccpy/flash/read_flash.py
@@ -1,5 +1,9 @@
+"""
+Code created by Peter Creasey
+"""
 from tables import openFile
-from numpy import mgrid, add, multiply, ones, flatnonzero, empty, float64, repeat
+from numpy import mgrid, add, multiply, ones, flatnonzero, empty, float64, repeat, int32, unique, arange
+from itertools import izip
 
 def read_flash_names(filename, names):
     """ read arrays of the given names in a flash file """
@@ -88,6 +92,104 @@ def get_data(filename, vars=('dens', 'pres', 'lamb', 'temp', 'eint')):
     result['time'] = data['time']
 
     return result
+
+def block_shape(f):
+    """
+    find the block shape (nxb, nyb, nzb) given the hdf5 file f
+    returns
+    dimension, (nxb, nyb, nzb)
+    """
+    
+    params =  f.getNode(f.root, 'integer scalars').read()
+    p_dict = dict((name.rstrip(), val) for name, val in params)
+
+    dimension = p_dict['dimensionality']
+    nb = empty(dimension, dtype=int32)
+    for i,par in enumerate(['nxb', 'nyb', 'nzb'][:dimension]):
+        nb[i]= p_dict[par] 
+
+    return dimension, nb
+
+def extent(f, dim):
+    """
+    return a (dim, 2) array of minimum and maximum value of x,y,z
+    """
+    
+    params =  f.getNode(f.root, 'real runtime parameters').read()
+    p_dict = dict((name.rstrip(), val) for name, val in params)
+
+    result = empty((dim,2), dtype=float64)
+
+    for i in range(dim):
+        dir = 'xyz'[i] 
+        result[i,0] = p_dict[dir+'min']
+        result[i,1] = p_dict[dir+'max']
+        
+    return result
+    
+def uniform_grid(filename, vars=['dens']):
+    """ quickly get uniform grids for the given variables """
+    f = openFile(filename, 'r')
+    
+
+    # shape of cells in block (e.g. (8,8,8))
+    dim, nb = block_shape(f)
+
+    block_sizes = f.getNode(f.root, 'block size').read()
+    nt = f.getNode(f.root, 'node type').read()
+    coords = f.getNode(f.root,'coordinates').read()
+        
+
+    # only want the leaf blocks
+    leaf_blocks = flatnonzero(nt==1)
+    coords = coords[leaf_blocks]
+
+    # check it is really a uniform grid
+    for i in range(dim):
+        if len(unique(block_sizes[:,i][leaf_blocks]))!=1:
+            raise Exception('Unequal leaf block sizes, not a uniformly adapted mesh')
+        
+    # assume uniform grid
+    dxyz = block_sizes[leaf_blocks[0]]
+    
+    # index into the uniform grid
+    idx = (coords/dxyz - 0.5).astype(int32)
+    
+    # shape of the uniform grid
+    grid_n = idx.max(0) + 1
+
+    uni_shape = nb * grid_n
+    print 'Uniform grid of shape', uni_shape
+
+
+    result = []
+    for var_name in vars:
+
+        flash_vals = f.getNode(f.root, var_name).read()
+
+        val_arr = empty(uni_shape[::-1], dtype= flash_vals.dtype)
+        for idx_i,val_block in izip(idx, flash_vals[leaf_blocks]):
+            a = nb * idx_i
+            b = nb * (idx_i+1)
+            
+            val_arr[a[2]:b[2], a[1]:b[1], a[0]:b[0]] = val_block
+
+        del flash_vals
+        result.append(val_arr)
+
+    min_max = extent(f, dim)
+    
+    f.close()
+
+    # make the x,y,z arrays
+    xyz = []
+    for i in range(dim):
+        dmin, dmax = min_max[i]
+        n_cells = uni_shape[i]
+        
+        xyz.append((arange(n_cells)+0.5) * ((dmax-dmin)/n_cells) + dmin)
+    
+    return tuple(xyz), result
 
 if __name__=='__main__':
     filename = '../Halo_hdf5_chk_0000'
