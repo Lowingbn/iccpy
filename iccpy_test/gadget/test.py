6280e09535ce815e0cd08ab2482574d32591e755
Ben Lowing <Lowingbn@gmail.com>
2012-10-04 14:51:26 +0000

    Updated tests

--- iccpy_test/gadget/test.py
+++ iccpy_test/gadget/test.py
@@ -1,15 +1,26 @@
 import unittest
-import iccpy.gadget.snapshot
+import iccpy.gadget
+import iccpy.gadget.binary_snapshot_io
 
-class GadgetSnapshotTestCase(unittest.TestCase):
+class GadgetBinaryIOTestCase(unittest.TestCase):
     
     def test_load_snapshot(self):
-        header, data = iccpy.gadget.snapshot.load_snapshot_file('data', 'gadget_binary_snapshot', 0)
+        header, data = iccpy.gadget.binary_snapshot_io.read_snapshot_file('data/gadget_binary_snapshot_000')
         
-        self.assertEqual(header['npart'][1], 2000)
+        self.assertEqual(header['num_particles'][1], 2000)
         self.assertEqual(len(data['pos']), 2000)
         self.assertEqual(len(data['vel']), 2000)
         self.assertEqual(len(data['id']), 2000)
     
+class GadgetSnapshotTestCase(unittest.TestCase):
+
+    def test_load_snapshot(self):
+        snapshot = iccpy.gadget.Snapshot(directory="data", filename="gadget_binary_snapshot", snapnum=0)
+
+        self.assertEqual(snapshot.header.num_particles[1], 2000)
+        self.assertEqual(len(snapshot.pos[1]), 2000)
+        self.assertEqual(len(snapshot.vel[1]), 2000)
+        self.assertEqual(len(snapshot.id[1]), 2000)
+
 if __name__ == '__main__':
     unittest.main()
